24bddde85eb154ccbe73d0454851f071c149b5a7
dimimall <malliaroy@gmail.com>
2025-04-09 11:19:24 +0300

    change pom xml

--- src/main/java/com/wocwithoneclick/wocwithoneclick/WocwithoneclickApplication.java
+++ src/main/java/com/wocwithoneclick/wocwithoneclick/WocwithoneclickApplication.java
@@ -3,7 +3,9 @@ package com.wocwithoneclick.wocwithoneclick;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    com.azure.spring.cloud.autoconfigure.implementation.context.AzureTokenCredentialAutoConfiguration.class
+  })
 public class WocwithoneclickApplication {
 
 	public static void main(String[] args) {
